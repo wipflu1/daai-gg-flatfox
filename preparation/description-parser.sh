9b7ab2ba4cffe60fc21e1fb35618d6c804f210f1
Grant-Gordon <ggmain777@gmail.com>
2024-07-04 10:08:03 +0200

    new branch for sentiment analysis, added updated sh/py analysis scripts

--- preparation/description-parser.sh
+++ preparation/description-parser.sh
@@ -2,12 +2,14 @@
 mkdir descriptions
 cd descriptions
 
-input_file="$1"
+input_file="$1"						#first argument is input_file = descriptions csv file
 i =1
 while IFS=',' read -r line; do
-	echo "$line" >description_${1}.txt
+	echo "$line" >description_${i}.txt		#create new txt file for each entry of csv
+	tr -d [:punt:] < description_${i}.txt		#remove punctuation from each entry
 	i++
 done < "$input_file"
 
-descriptions_list=(*.txt)
-python3 sentimentAnalysis.py "${descriptions_list[@]}"
+descriptions_list=(description_[0-9]{0-10}.txt)			#Create list of all txt files
+python3 sentimentAnalysis.py "$1 ${descriptions_list[@]}"	#Call python script for analysis on all txt files
+rm description_[0-9]{0-10}.txt
--- preparation/sentimentAnalysis.py
+++ preparation/sentimentAnalysis.py
@@ -1,16 +1,22 @@
 import sys
-
+import csv
 def sentimentAnalysis():
-    with open(file_path, 'r') as file:
-        data = file.read()
-        #analysis
+    totalSentiment=0                                        #total sentiment value of the txt file
+    with open(file_path, 'r') as file:                      #open txt file
+        data = file.read()                                  #data = text within txt file
+        words =  data.split()                               #words = list of words within data
+        for word in words:                                  #itterate words 
+            with open('sentiment.csv', 'rt') as file:       #assume sentiment.csv = csv of swiss words
+                reader = csv.reader(file, delimeter=',')    #copied from stackoverflow
+                for row in reader:                          #^
+                    if word == row[0]:                      #assuming row[0] = word
+                        totalSentiment+=row[1]              # assuming row[1] = word's sentiment value
 
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("invalid number of arguments")
+if __name__ == "__main__":                                  #if called from sh script??
+    if len(sys.argv) < 2:                                   #error if less than 1 arguement
+        print("invalid number of arguments")                
         sys.exit(1)
-    for file_path in sys.argv[1:]:
-        stringSentiment(file_path)
+    for file_path in sys.argv[1:]:                          #call sentimentAnalysis for every arguement provided
+        sentimentAnalysis(file_path)
         
 

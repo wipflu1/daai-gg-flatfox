09bb06703196d66f53c4b940b4820eaab0d55afd
Grant-Gordon <ggmain777@gmail.com>
2024-07-04 11:28:35 +0200

    added sentiment analysis files

---
+++ sentiment_analysis/description-parser.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+mkdir descriptions
+cd descriptions
+
+input_file="$1"						#first argument is input_file = descriptions csv file
+i =1
+while IFS=',' read -r line; do
+	echo "$line" >description_${i}.txt		#create new txt file for each entry of csv
+	tr -d [:punt:] < description_${i}.txt		#remove punctuation from each entry
+	tr -d \[tn]
+	i++
+done < "$input_file"
+
+descriptions_list=(description_"*/d".txt)			#Create list of all txt files
+python3 sentimentAnalysis.py "$1 ${descriptions_list[@]}"	#Call python script for analysis on all txt files
+rm description_"*/d".txt

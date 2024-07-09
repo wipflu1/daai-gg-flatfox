26d559cb03e698cf255cf8ae4e738daef3970b98
Grant-Gordon <ggmain777@gmail.com>
2024-07-09 10:08:38 +0200

    still not functional but compiling. bash script reads each line as new text file. abouot to attempt to alter descriptions csv to remove punctuation

--- sentiment_analysis/description-parser.sh
+++ sentiment_analysis/description-parser.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 mkdir descriptions
-cd descriptions
 
 input_file="$1"						#first argument is input_file = descriptions csv file
-i =1
+i=1
 while IFS=',' read -r line; do
-	echo "$line" >description_${i}.txt		#create new txt file for each entry of csv
-	tr -d [:punt:] < description_${i}.txt		#remove punctuation from each entry
-	tr -d \[tn]
-	i++
+	cd descriptions
+	echo "$line" >description_text_num_${i}.txt		#create new txt file for each entry of csv
+	tr -d [:punct:] < description_text_num_${i}.txt		#remove punctuation from each entry
+	tr -d \[tn] < description_text_num_${i}.txt
+	i=$((i+1))
+	echo description text file: "$i" created
+	cd ..	
 done < "$input_file"
 
-descriptions_list=(description_"*/d".txt)			#Create list of all txt files
-python3 sentimentAnalysis.py "$1 ${descriptions_list[@]}"	#Call python script for analysis on all txt files
-rm description_"*/d".txt
+descriptions_list=(description_text_num_*)			#Create list of all txt files
+echo  calling sentiment analysis with : $1 ${descriptions_list[@]}
+python3 ../sentimentAnalysis.py "$1 ${descriptions_list[@]}"	#Call python script for analysis on all txt files
+rm description_text_num_*
--- sentiment_analysis/sentimentAnalysis.py
+++ sentiment_analysis/sentimentAnalysis.py
@@ -1,22 +1,30 @@
 import sys
 import csv
-def sentimentAnalysis():
+def sentimentAnalysis(file_path, argNum):
     totalSentiment=0                                        #total sentiment value of the txt file
     with open(file_path, 'r') as file:                      #open txt file
         data = file.read()                                  #data = text within txt file
         words =  data.split()                               #words = list of words within data
         for word in words:                                  #itterate words 
-            with open('sentiment.csv', 'rt') as file:       #assume sentiment.csv = csv of swiss words
+            with open('englishGerman.csv', 'rt') as file:   #assume englishGerman.csv = csv of german and english: word, val
                 reader = csv.reader(file, delimeter=',')    #copied from stackoverflow
                 for row in reader:                          #^
                     if word == row[0]:                      #assuming row[0] = word
                         totalSentiment+=row[1]              # assuming row[1] = word's sentiment value
+    if(totalSentiment==0):
+        totalSentiment = "NA"
 
+        
+    with open(descriptionOut.csv, 'w') as csvfile:
+        writer = csv.writer(csvfuke,fieldnames=['description_ID','senitment_value']
+        writer.writeheader()
+        writer.writerows([argNum, totalSentiment])
+`
 if __name__ == "__main__":                                  #if called from sh script??
     if len(sys.argv) < 2:                                   #error if less than 1 arguement
         print("invalid number of arguments")                
         sys.exit(1)
     for file_path in sys.argv[1:]:                          #call sentimentAnalysis for every arguement provided
-        sentimentAnalysis(file_path)
+        sentimentAnalysis(file_path, sys.argv[file_path]))
         
 
